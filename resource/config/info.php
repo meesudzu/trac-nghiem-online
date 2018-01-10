53655e0cc9bd8a3aba3c7dbe45dc6dd35e9fca59
Dzu <meesudzu@gmail.com>
2018-01-10 14:20:23 +0700

    UPDATE
    update config + info

--- resource/config/info.php
+++ resource/config/info.php
@@ -9,7 +9,7 @@ class Info()
 {
 	//biến lưu thông tin chung của trang web
 	final $title = "Hệ Thống Trắc Nghiệm Online";
-	final $copyright = "Nhóm phát triển:<br />Nông Văn Du (dzu6996@gmail.com)<br />Trịnh Văn Dương<br />Trần Văn Huy<br />Vương Văn Huy<br />Hoàng Phương Nam";
+	final $copyright = "Hoàn thành và nộp ngày 20/09/2017<br />Lớp KTPM-K14C";
 	final $version = "2.1.0";
 	final $contributes = "Nhóm phát triển:<br />Nông Văn Du (dzu6996@gmail.com)<br />Trịnh Văn Dương<br />Trần Văn Huy<br />Vương Văn Huy<br />Hoàng Phương Nam";
 	final $bio = "";
