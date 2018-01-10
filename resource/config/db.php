fa65743a264dea056b52fffa1bf1c2b87c168f2e
Dzu <meesudzu@gmail.com>
2018-01-10 14:18:33 +0700

    Revert "UPDATE"
    This reverts commit fa425ed71894904569c66daeee22e416e399811c.

--- resource/config/db.php
+++ resource/config/db.php
@@ -1,6 +1,5 @@
 <?php
 
-
 /**
  * DATABASE
  * Author: Dzu
@@ -9,11 +8,15 @@
 class Database{
 	private	$db = '';
 	private $sql = '';
-
 	// hàm kết nối cơ sở dữ liệu
 	public function Database()
 	{
 		try{
+			/*host: tên host
+			dbname: tên database
+			root: tên user trong mysql
+			mật khẩu bỏ trống
+			kiểu encoding utf8*/
 			$this->db = new PDO('mysql:host=localhost; dbname=tracnghiem_online','root','');
 			$this->db->query('set names "utf8"');
 		}
--- resource/model/m_admin.php
+++ resource/model/m_admin.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once ('db.php');
+include_once ('config/db.php');
 
 /**
  * Model Admin
