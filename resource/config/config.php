6f5b7fa064a5b5ef7f83d413e47a0fa6a1645c69
meesudzu <dzu6996@gmail.com>
2019-01-15 11:23:45 +0700

    Pre-Release 3.4.0 fix path sửa câu hỏi

--- resource/config/config.php
+++ resource/config/config.php
@@ -10,10 +10,10 @@ class Config
 {
 	const TITLE = 'Hệ Thống Trắc Nghiệm Online';
 	const COPYRIGHT = "Copyright © 2016 Nong Van Du";
-	const VERSION = "3.3.0";
+	const VERSION = "Pre-Release 3.4.0";
 	const OWNER = "Nông Văn Du (Dzu)";
 	const CONTRIBUTOR = "";
 	const EMAIL = "dzu6996@gmail.com";
-	const RELEASE = "16/11/2018";
+	const RELEASE = "xx/xx/2019";
 	const TIMEZONE = "Asia/Bangkok";
 }
