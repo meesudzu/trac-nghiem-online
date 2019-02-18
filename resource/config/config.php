727418ad1b6d5922bef0aa743782080d134b0e32
Du <dzu6996@gmail.com>
2019-02-18 23:18:48 +0700

    Release 3.4.0

--- resource/config/config.php
+++ resource/config/config.php
@@ -10,10 +10,10 @@ class Config
 {
 	const TITLE = 'Hệ Thống Trắc Nghiệm Online';
 	const COPYRIGHT = "Copyright © 2016 Nong Van Du";
-	const VERSION = "Pre-Release 3.4.0";
+	const VERSION = "Release 3.4.0";
 	const OWNER = "Nông Văn Du (Dzu)";
 	const CONTRIBUTOR = "";
 	const EMAIL = "dzu6996@gmail.com";
-	const RELEASE = "xx/xx/2019";
+	const RELEASE = "18/02/2019";
 	const TIMEZONE = "Asia/Bangkok";
 }
