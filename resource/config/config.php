12dadf0ff0ca046765ea4d1e4483cb4e68d18bfd
Du <dzu6996@gmail.com>
2020-05-19 21:03:14 +0700

    Update config.php

--- resource/config/config.php
+++ resource/config/config.php
@@ -12,11 +12,11 @@ class Config
 {
     const TITLE = 'Hệ Thống Trắc Nghiệm Online';
     const COPYRIGHT = "Copyright © 2016 Nong Van Du";
-    const VERSION = "3.4.2 #8";
+    const VERSION = "3.4.3";
     const OWNER = "Nông Văn Du (Dzu)";
     const CONTRIBUTOR = "";
     const EMAIL = "dzu6996@gmail.com";
-    const RELEASE = "27/03/2019";
+    const RELEASE = "15/05/2020";
     const TIMEZONE = "Asia/Bangkok";
     const APP_URL = "http://localhost/";
 }
